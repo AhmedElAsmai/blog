2045d74dc88b8fe587cc7427a29c0ca0554a8e10
ahmedelasmai <ahmed.elasmai@outlook.com>
2024-04-04 13:17:56 +0100

    completed basic authentication

--- app.py
+++ app.py
@@ -1,17 +1,32 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
 import sqlite3
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
 def index():
-    db = db_class()
     messages = db.messages() 
     return render_template('index.html',messages=messages)
 
+@app.route('/login',methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        authentication = db.authentication(username, password)
+        
+        if authentication:
+            return redirect('/index')
+    return render_template('login.html')
+
+# @app.route('/register')
+# def register():
+#     return render_template('register.html')
 
 class db_class:
     def messages(self, start_index=1, num_rows=20):
+        
         with sqlite3.connect('blog.db') as conn:
             cursor = conn.cursor()       
             cursor.execute("SELECT * FROM messages LIMIT ? OFFSET ?", (num_rows, start_index - 1))  
@@ -19,7 +34,18 @@ class db_class:
             cursor.close()
         
         return messages
-   
+    
+    def authentication(self, username, password):
+        with sqlite3.connect('blog.db') as conn:
+            cursor = conn.cursor()       
+            cursor.execute("SELECT * FROM users WHERE username = ? AND password = ?", (username, password))  
+            user = cursor.fetchone()
+            cursor.close()       
+
+        return user
+        
+
+db = db_class()
 
 
 
